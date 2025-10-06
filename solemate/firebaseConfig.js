ee85475d0880979af8442da1732a413f30b71c31
Shaswath <shaswathgiridhran@gmail.com>
2025-10-06 14:53:04 +0530

    images are saving in firestore

---
+++ solemate/firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAOHTViBRXaO3BRG_7EtfVDX9hlGHo3EiQ",
+  authDomain: "solemate-dff15.firebaseapp.com",
+  projectId: "solemate-dff15",
+  storageBucket: "solemate-dff15.firebasestorage.app",
+  messagingSenderId: "793379416835",
+  appId: "1:793379416835:web:f070b1a3b14c19816dd736",
+  measurementId: "G-PSZEKLMNPL"
+};
+
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
